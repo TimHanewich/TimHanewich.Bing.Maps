7c9a7cb68367f9aef5a61d952dd5e617bd29d71e
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-08-21 08:53:16 -0700

    Added Pushpin construtor method

---
+++ FunctionalTesting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TimHanewich.Bing.Maps;
+
+namespace FunctionalTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BingMapsPushpinsImageryRequest req = new BingMapsPushpinsImageryRequest();
+            req.Width = 300;
+            req.Height = 200;
+            req.ImagerySet = ImageryType.Aerial;
+
+            
+        }
+    }
+}
--- src/Imagery/Pushpin.cs
+++ src/Imagery/Pushpin.cs
@@ -9,6 +9,19 @@ namespace TimHanewich.Bing.Maps
         public int Style {get; set;} //https://docs.microsoft.com/en-us/bingmaps/rest-services/common-parameters-and-types/pushpin-syntax-and-icon-styles
         public string Label {get; set;}
 
+        public Pushpin()
+        {
+
+        }
+
+        public Pushpin(float lat, float lon, int style, string label)
+        {
+            Latitude = lat;
+            Longitude = lon;
+            Style = style;
+            Label = label;
+        }
+
         public string AsUrlRequestComponent()
         {
             string ToReturn = "";
