b0a9d7b3e4e7431cf8c8701e64c05be847c513b2
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-08-21 09:03:49 -0700

    Did some functional testing

--- FunctionalTesting/Program.cs
+++ FunctionalTesting/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using TimHanewich.Bing.Maps;
+using System.Collections.Generic;
+using System.IO;
 
 namespace FunctionalTesting
 {
@@ -7,11 +9,26 @@ namespace FunctionalTesting
     {
         static void Main(string[] args)
         {
+            BingMapsApiHelper apiHelper = new BingMapsApiHelper("");
+
             BingMapsPushpinsImageryRequest req = new BingMapsPushpinsImageryRequest();
-            req.Width = 300;
-            req.Height = 200;
+            req.Width = 800;
+            req.Height = 800;
             req.ImagerySet = ImageryType.Aerial;
 
+            List<Pushpin> pps = new List<Pushpin>();
+            pps.Add(new Pushpin(47.493266f, -122.215553f, 1, "SEA"));
+            pps.Add(new Pushpin(47.908243f, -122.285577f, 1, "PAI"));
+            req.Pushpins = pps.ToArray();
+
+            Stream s = apiHelper.DownloadPushpinsImageryAsync(req).Result;
+
+            string path = "C:\\Users\\tihanewi\\Downloads\\img.jpg";
+            Stream towrite = System.IO.File.OpenWrite(path);
+            s.Position = 0;
+            s.CopyTo(towrite);
+            towrite.Dispose();
+            s.Dispose();
             
         }
     }
