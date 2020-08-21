c193c1e1fb8d5fffd3ad396d34d98f341355e4ec
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-08-21 08:31:14 -0700

    Added AsUrlRequestComponent method to Pushpin

--- src/Imagery/Pushpin.cs
+++ src/Imagery/Pushpin.cs
@@ -8,5 +8,12 @@ namespace TimHanewich.Bing.Maps
         public float Longitude {get; set;}
         public int Style {get; set;} //https://docs.microsoft.com/en-us/bingmaps/rest-services/common-parameters-and-types/pushpin-syntax-and-icon-styles
         public string Label {get; set;}
+
+        public string AsUrlRequestComponent()
+        {
+            string ToReturn = "";
+            ToReturn = Latitude.ToString() + "," + Longitude.ToString() + ";" + Style.ToString() + ";" + Label;
+            return ToReturn;
+        }
     }
 }
