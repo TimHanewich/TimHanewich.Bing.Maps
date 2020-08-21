ee856192ac8cf1d8adf72f1b18827214e3528f87
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-08-21 08:47:23 -0700

    Added method to the BingMapsApiHelper for downloading pushpin maps imagery

--- src/BingMapsApiHelper.cs
+++ src/BingMapsApiHelper.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
 
 namespace TimHanewich.Bing.Maps
 {
@@ -10,8 +15,41 @@ namespace TimHanewich.Bing.Maps
         {
             ApiKey = api_key;
         }
-    
         
+        #region "Imagery"
+
+        public async Task<Stream> DownloadPushpinsImageryAsync(BingMapsPushpinsImageryRequest request)
+        {
+            string url = "https://dev.virtualearth.net/REST/v1/Imagery/Map/";
+
+            //The imagery type
+            url = url + request.ImagerySet.ToString().ToLower() + "?";
+
+            //Add in all the pushpins
+            foreach (Pushpin pp in request.Pushpins)
+            {
+                url = url + "pushpin=" + pp.AsUrlRequestComponent() + "&";
+            }
+
+            //Size of the image
+            url = url + "mapsize=" + request.Width.ToString() + "," + request.Height.ToString() + "&";
+
+            //Key
+            url = url + "key=" + ApiKey;
+
+
+            //Make the request and get the Stream
+            HttpClient hc = new HttpClient();
+            HttpResponseMessage hrm = await hc.GetAsync(url);
+            if (hrm.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("Request to the Bing Maps API failed.");
+            }
+            Stream s = await hrm.Content.ReadAsStreamAsync();
+            return s;
+        }
+
+        #endregion
     
     }
 }
