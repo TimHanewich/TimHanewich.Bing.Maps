f5dac3a71ecee66d392f4120a91336a5793d0954
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-08-21 07:30:36 -0700

    Initial commit

---
+++ src/BingMapsApiHelper.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TimHanewich.Bing.Maps
+{
+    public class BingMapsApiHelper
+    {
+        private string ApiKey;
+
+        public BingMapsApiHelper(string api_key)
+        {
+            ApiKey = api_key;
+        }
+    
+        
+    
+    }
+}
