aca9a080bc0ebd92439b750c70cfcb6d88b629ae
Tim Hanewich <tahanewich@live.com>
2021-11-09 20:39:11 -0500

    Added ZoomLevel

--- src/Imagery/BingMapsImageryRequest.cs
+++ src/Imagery/BingMapsImageryRequest.cs
@@ -4,6 +4,7 @@ namespace TimHanewich.Bing.Maps
 {
     public class BingMapsImageryRequest
     {
+        public int ZoomLevel {get; set;} //Zoom level from 1 (far away) to 20 (up close)
         public int Width {get; set;}
         public int Height {get; set;}
     }
